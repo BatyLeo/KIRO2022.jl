4bf6717ef918eeb626d75960f1f91906aed872f4
Documenter.jl <documenter@juliadocs.github.io>
2022-12-01 08:12:48 +0000

    build based on 9a2290a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KIRO2022","category":"page"},{"location":"#KIRO2022","page":"Home","title":"KIRO2022","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KIRO2022.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KIRO2022]","category":"page"}]
+}
