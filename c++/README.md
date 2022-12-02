863199bdb75d5d868f289da6e3f98ed99b2d5781
BatyLeo <leo.baty67@gmail.com>
2022-12-02 16:58:57 +0100

    update readme

---
+++ c++/README.md
@@ -0,0 +1,12 @@
+# How to run ?
+
+- Need to have c++ 20 installed
+- Need to have cmake installed
+
+```bash
+mkdir build
+cd build
+cmake ..
+make
+./kiro2022
+```
--- c++/main.cpp
+++ c++/main.cpp
@@ -5,14 +5,13 @@
 
 using namespace std;
 
-
 int main() {
-    Instance instance = read_instance("../../instances/tiny.json");
+    Instance instance = read_instance("../../instances/KIRO-tiny.json");
     cout << instance << endl;
     Solution solution = read_solution("../../solutions/KIRO-tiny-sol_42.json");
     cout << solution << endl;
     write_solution(solution, "../../sol.json");
-    cout << "Is feasible? " << is_feasible(solution, instance) << endl;
+    cout << "Is feasible? " << is_feasible(solution, instance) << endl; // Note: implementation of this function is unfinished
     cout << "Solution cost? " << cost(solution, instance) << endl;
     return 0;
 }
